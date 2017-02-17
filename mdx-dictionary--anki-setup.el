50a56217fc1a344b27dccd3cd8dc43a018f25feb
lujun9972 <lujun9972@gmail.com>
2017-02-17 17:29:05 +0800

    mdx-dictionary查询字典后记录到文本中,以便导入anki

---
+++ mdx-dictionary--anki-setup.el
@@ -0,0 +1,14 @@
+(require 'f)
+
+(defun mdx-dictionary--save-to-anki (content)
+  (let* ((word (word-at-point))
+         (sentence (replace-regexp-in-string "[\r\n]+" " " (sentence-at-point))) ;去掉句子中的断行
+         (sentence (replace-regexp-in-string (regexp-quote word)
+                                             (lambda (word)
+                                               (format "<b>%s</b>" word))
+                                             sentence)) ;高亮句子中的单词
+         (content (replace-regexp-in-string "[\r\n]+" "<br>" content)))
+    (f-append (format "%s|%s|%s\n" word content sentence) 'utf-8  "~/mdx-dictionary-for-anki.txt")))
+
+(add-hook 'mdx-dictionary-display-before-functions #'mdx-dictionary--save-to-anki)
+
